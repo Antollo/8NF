77e7d8b866e4d40322a8400fc625d29630ec92fb
Antoni Nowinowski <antoninowinowski@hotmail.com>
2018-10-10 22:10:46 +0200

    2018 autumn update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var mongodb = require('mongodb');
 var MongoClient = mongodb.MongoClient;
 var url = process.env.BASE;
-console.log(url);
+console.log();
 
 var express = require('express');
 var app = express();
@@ -13,42 +13,52 @@ var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: true });
 var port = process.env.PORT || 3000;
 
+var nodemailer = require('nodemailer');
+var botMailAddress = 'lenezjastankoska@gmail.com';
+var appAddress = 'https://noc-filmowa.herokuapp.com/';
+//var appAddress = 'http://localhost:3000/';
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: botMailAddress,
+        pass: process.env.BASE.substring(19, 31)
+    }
+});
+
 var films = [];
+var codeToVote = new Map();
 read();
 
-var line = `
-<tr>
-    <td class="mdl-data-table__cell--non-numeric"></td>
-    <td></td>
-    <td class="mdl-data-table__cell--non-numeric">
-        <button class="mdl-button mdl-js-button mdl-button--icon">
-            <i class="material-icons">language</i>
-        </button>
-    </td>
-    <td class="mdl-data-table__cell--non-numeric">
-        <button class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored">
-            <i class="material-icons">thumb_up</i>
-        </button>
-    </td>
-</tr>`
+var lineHTML, responseHTML;
+fs.readFile(__dirname + '/line.html', 'utf8', function (err, data) {
+    if (err) throw err;
+    lineHTML = data;
+    console.log('row.html loaded');
+});
+fs.readFile(__dirname + '/res.html', 'utf8', function (err, data) {
+    if (err) throw err;
+    responseHTML = data;
+    console.log('res.html loaded');
+});
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-    fs.readFile(__dirname + '/public/main.html', 'utf8', function(err, data) {
+    fs.readFile(__dirname + '/public/main.html', 'utf8', function (err, data) {
         if (err) throw err;
         $ = cheerio.load(data);
-        films.sort(function(b, a){return Object.keys(a.votes).length - Object.keys(b.votes).length});
-        films.forEach(function(film) {
-            var newFilm = $(line);
+        films.sort(function (b, a) { return Object.keys(a.votes).length - Object.keys(b.votes).length });
+        films.forEach(function (film) {
+            var newFilm = $(lineHTML);
             newFilm.children().eq(0).html('<div class="inner">' + film.title + '</div>');
             newFilm.children().eq(1).text(Object.keys(film.votes).length);
             newFilm.children().eq(2).children().attr('onclick', 'location.href="' + film.link + '";');
-            newFilm.children().eq(3).children().attr('onclick', '$.post("vote", {title: "' + film.title + '", fingerprint: key}, function(data, status){ $(".mdl-js-snackbar")[0].MaterialSnackbar.showSnackbar({message: data});re();});');
+            newFilm.children().eq(3).children().attr('onclick', '$.post("vote", {title: "' + film.title + '", mail: mail}, function(data, status){ $(".mdl-js-snackbar")[0].MaterialSnackbar.showSnackbar({message: data});});');
             $('#table').append(newFilm);
         });
         data = '<html>' + $('html').html() + '</html>'
-        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(data);
         res.end();
     });
@@ -58,45 +68,86 @@ app.get('/base', function (req, res) {
     res.send(films);
 });
 
+
 app.post('/add', urlencodedParser, function (req, res) {
     var film = {
         title: $("<div>").html(req.body.title).text(),
-        link: $("<div>").html(req.body.link).text().replace('"','').replace(';',''),
+        link: $("<div>").html(req.body.link).text().replace('"', '').replace(';', ''),
         votes: {}
     };
     films.push(film);
-    res.send('Dodawanie udało się.');
-    console.log(films);
+    res.send('Dodawanie udało się');
     save();
 });
 
-app.post('/vote', urlencodedParser, function (req, res) {
-    var result = films.filter(function(film) {
-        return film.title == $("<div>").html(req.body.title).text();
+app.get('/validate/:key', function (req, res) {
+    var filmTitle = codeToVote.get(req.params.key);
+    console.log(filmTitle);
+    var result = films.filter(function (film) {
+        return film.title === filmTitle;
     });
-    result.forEach(function(film) {
-        if(film.votes[$("<div>").html(req.body.fingerprint).text()] === undefined) {
-            film.votes[$("<div>").html(req.body.fingerprint).text()] = 1;
-            res.send('Głosowanie udało się.');
+    result.forEach(function (film) {
+        if (film.votes[req.params.key] === undefined) {
+            film.votes[req.params.key] = 1;
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(responseHTML.replace('Wystąpił błąd.', 'Głosowanie udało się.'));
+            res.end();
             save();
         } else {
-            res.send('Już na to głosowałeś!');
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(responseHTML.replace('Wystąpił błąd.', 'Już na to głosowałaś/głosowałeś!'));
+            res.end();
         }
     });
+    if(result.length == 0) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(responseHTML.replace('Wystąpił błąd.', 'Głosowanie nie udało się.'));
+            res.end();
+    }
+});
+
+app.post('/vote', urlencodedParser, function (req, res) {
+    var filmTitle = $("<div>").html(req.body.title).text();
+    var userMail = req.body.mail;
+    if (userMail.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@(hotmail.com|gmail.com)$/)) {
+        var hashed = hash(filmTitle+userMail);
+        transporter.sendMail(
+        {
+            from: botMailAddress,
+            to: userMail,
+            subject: 'Potwierdzenie głosu na film',
+            html: 'Hej!<br>Jestem botem odpowiedzialnym za głosowanko na filmy, które będą na nocce. Żeby potwierdzić swój głos kliknij tutaj: '
+            + '<a href="' + appAddress + 'validate/' + hashed.toString() + '">' + appAddress + 'validate/'
+            + hashed.toString() + '</a><br>Link jest ważny przez pół godziny więc lepiej się pospiesz.<br>Do zobaczenia na nocy!'
+        }, function (error, info) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        });
+        res.send('Wysłano mail z linkiem do potwierdzenia');
+        codeToVote.set(hashed.toString(), filmTitle);
+        setTimeout(function () {
+            codeToVote.delete(hashed);
+        }, 1000*60*30);
+    } else {
+        res.send('Twój adres mailowy jest niepoprawny');
+    }
 });
 
 var server = app.listen(port, function () {
 });
 
 function save() {
-	MongoClient.connect(url, function(err, db) {
-      if (err) throw err;
-      var dbo = db.db("noc-filmowa");
-      dbo.collection("table").updateOne({}, {$set: {table: films}}, function(err, res) {
-          if (err) throw err;
-          console.log("1 document updated");
-          db.close();
-      });
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("noc-filmowa");
+        dbo.collection("table").updateOne({}, { $set: { table: films } }, function (err, res) {
+            if (err) throw err;
+            console.log("1 document updated");
+            db.close();
+        });
     });
     //fs.writeFile('base.txt', JSON.stringify(films), function (err) {
     //    if (err) throw err;
@@ -105,10 +156,10 @@ function save() {
 }
 
 function read() {
-	MongoClient.connect(url, function(err, db) {
+    MongoClient.connect(url, function (err, db) {
         if (err) throw err;
         var dbo = db.db("noc-filmowa");
-        dbo.collection("table").findOne({}, function(err, result) {
+        dbo.collection("table").findOne({}, function (err, result) {
             if (err) throw err;
             films = result.table;
             db.close();
@@ -120,3 +171,12 @@ function read() {
     //    console.log('Done');
     //});
 }
+
+function hash(str) {
+    var h = 0;
+    for (var i = 0; i < str.length; i++) {
+        h = ((h << 5) - h) + str.charCodeAt(i);
+        h |= 0;
+    }
+    return h;
+};
--- public/main.js
+++ public/main.js
@@ -1,96 +1,86 @@
-var key;
+var mail;
 $(document).ready(function () {
-    setTimeout(function () {
-        if(window['canRunAds'] === undefined) 
-        {
-            $('.mdl-js-snackbar')[0].MaterialSnackbar.showSnackbar({message: 'Wyłącz adblock\'a i odśwież stronę. Adblock blokuje skrypty niezbędne do identyfikacji użytkownika.', actionHandler: function(event) {location.reload();}, actionText: 'Odśwież', timeout: 60000});
-        } else {
-            $('#loading-screen').hide();
-            $('#main-screen').show();
-            if(Cookies.get('session') == 'active') {
-                $('#info-card').hide();
-                $('#new-card').hide();
-                $('#ranking-card').show();
-                $('#chart-card').show();
-                new Fingerprint2().get(function(result, components) {
-                    key = result;
-                })
-                startChart();
-            }
-        }
-    }, 200);
-
-    $('#agree-button').click(function() {
+    function startTable () {
         $('#info-card').hide();
         $('#new-card').hide();
         $('#ranking-card').show();
         $('#chart-card').show();
-        Cookies.set('session','active');
         startChart();
-    });
+    }
+    setTimeout(function () {
+        //$('.mdl-js-snackbar')[0].MaterialSnackbar.showSnackbar({message: 'Wyłącz adblock\'a i odśwież stronę. Adblock blokuje skrypty niezbędne do identyfikacji użytkownika.', actionHandler: function(event) {location.reload();}, actionText: 'Odśwież', timeout: 60000});
+        $('#loading-screen').hide();
+        $('#main-screen').show();
+        if (Cookies.get('mail') !== undefined) {
+            startTable();
+            mail = Cookies.get('mail');
+        }
+    }, 100);
 
-    $('#floating').click(function() {
-        $('#info-card').hide();
-        $('#new-card').show();
-        $('#ranking-card').hide();
-    });
-
-    $('#add').click(function() {
-        if($("<div>").html($('#title').val()).text().length !=0 && $("<div>").html($('#link').val()).text().length != 0 && $("<div>").html($('#link').val()).text().indexOf('http') != -1 && !$('#link').parent().hasClass('is-invalid'))
-        {
-            $.post("add",
-            {
-                title: $('#title').val(),
-                link: $('#link').val()
-            },
-            function(data, status){
-                $('.mdl-js-snackbar')[0].MaterialSnackbar.showSnackbar({message: data});
-                re()
-            });
-        } else {
-            $('.mdl-js-snackbar')[0].MaterialSnackbar.showSnackbar({message: 'Tytuł jest pusty lub link nieprawidłowy.'});
+    $('#agree-button').click(function () {
+        if ($('#mail').val().length > 0 && !$('#mail').parent().hasClass('is-invalid')) {
+            startTable();
+            Cookies.set('mail', $('#mail').val());
+            mail = $('#mail').val();
+            $('#main-screen').scrollTop(0);
         }
     });
 
-    $('#back').click(function() {
+    $('#floating').click(function () {
+        $('#info-card').hide();
+        $('#new-card').show();
+        $('#ranking-card').hide();
+        $('#chart-card').hide();
+        $('#main-screen').scrollTop(0);
+    });
+
+    $('#add').click(function () {
+        if ($("<div>").html($('#title').val()).text().length != 0 && $("<div>").html($('#link').val()).text().length != 0 && $("<div>").html($('#link').val()).text().indexOf('http') != -1 && !$('#link').parent().hasClass('is-invalid')) {
+            $.post("add",
+                {
+                    title: $('#title').val(),
+                    link: $('#link').val()
+                },
+                function (data, status) {
+                    $('.mdl-js-snackbar')[0].MaterialSnackbar.showSnackbar({ message: data });
+                    re()
+                });
+        } else {
+            $('.mdl-js-snackbar')[0].MaterialSnackbar.showSnackbar({ message: 'Tytuł jest pusty lub link nieprawidłowy.' });
+        }
+    });
+
+    $('#back').click(function () {
         $('#info-card').hide();
         $('#new-card').hide();
         $('#ranking-card').show();
+        $('#chart-card').show();
     });
-
-    console.log(`
-"Ty pojedziesz w daleką, nieznajomą drogę;
-Będziesz w wielkich, bogatych i rozumnych tłumie,
-Szukaj męża, co więcej niźli oni umie;
-Poznasz, bo cię powita pierwszy w Imię Boże.
-Słuchaj, co powie…"
-    Adam Mickiewicz "Dziady", część III
-    `);
 });
 
 function re() {
     $('#loading-screen').show();
     $('#main-screen').hide();
-    setTimeout(function() {
+    setTimeout(function () {
         location.reload();
     }, 2500);
 }
 
 function startChart() {
-    //Draw  top 10 chart
-    google.charts.load('current', {'packages':['bar']});
+    //Draw top 10 chart
+    google.charts.load('current', { 'packages': ['bar'] });
     google.charts.setOnLoadCallback(drawChart);
     function drawChart() {
         var data = new google.visualization.DataTable();
         data.addColumn('string', 'Tytuł');
         data.addColumn('number', 'Głosy');
-        $.each($("#table > tr"), function(key, value) {
+        $.each($("#table > tr"), function (key, value) {
             if (key < 10) data.addRow([$(value).children(':nth-child(1)').text(), parseInt($(value).children(':nth-child(2)').text())]);
         })
         var options = {
             chart: {},
             bars: 'horizontal',
-            legend: {position: 'none'},
+            legend: { position: 'none' },
             colors: ['rgb(233,30,99)']
         };
         var chart = new google.charts.Bar(document.getElementById('chart-div'));
