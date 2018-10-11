79a30d40eb17549aa4ebbd6db9dc085474107170
Antoni Nowinowski <antoninowinowski@hotmail.com>
2018-10-11 17:27:00 +0200

    Better db operations

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var mongodb = require('mongodb');
 var MongoClient = mongodb.MongoClient;
 var url = process.env.BASE;
-console.log();
 
 var express = require('express');
 var app = express();
@@ -26,9 +25,7 @@ var transporter = nodemailer.createTransport({
     }
 });
 
-var films = [];
 var codeToVote = new Map();
-read();
 
 var lineHTML, responseHTML;
 fs.readFile(__dirname + '/line.html', 'utf8', function (err, data) {
@@ -48,24 +45,69 @@ app.get('/', function (req, res) {
     fs.readFile(__dirname + '/public/main.html', 'utf8', function (err, data) {
         if (err) throw err;
         $ = cheerio.load(data);
-        films.sort(function (b, a) { return Object.keys(a.votes).length - Object.keys(b.votes).length });
-        films.forEach(function (film) {
-            var newFilm = $(lineHTML);
-            newFilm.children().eq(0).html('<div class="inner">' + film.title + '</div>');
-            newFilm.children().eq(1).text(Object.keys(film.votes).length);
-            newFilm.children().eq(2).children().attr('onclick', 'location.href="' + film.link + '";');
-            newFilm.children().eq(3).children().attr('onclick', '$.post("vote", {title: "' + film.title + '", mail: mail}, function(data, status){ $(".mdl-js-snackbar")[0].MaterialSnackbar.showSnackbar({message: data});});');
-            $('#table').append(newFilm);
+
+        MongoClient.connect(url, function (err, db) {
+            if (err) throw err;
+            var dbo = db.db('noc-filmowa');
+            var cursor = dbo.collection('films').aggregate([
+                {
+                    $project: {
+                        title: 1,
+                        link: 1,
+                        votes: 1,
+                        votesCount: { $size: '$votes' }
+                    }
+                },
+                { $sort: { votesCount: -1 } },
+                { $limit: 500 }
+            ]);
+            cursor.on("data", function (data) {
+                var newFilm = $(lineHTML);
+                newFilm.children().eq(0).html('<div class="inner">' + data.title + '</div>');
+                newFilm.children().eq(1).text(Object.keys(data.votes).length);
+                newFilm.children().eq(2).children().attr('onclick', 'location.href="' + data.link + '";');
+                newFilm.children().eq(3).children().attr('onclick', '$.post("vote", {title: "' + data.title + '", mail: mail}, function(data, status){ $(".mdl-js-snackbar")[0].MaterialSnackbar.showSnackbar({message: data});});');
+                $('#table').append(newFilm);
+            });
+            cursor.on("end", function () {
+                db.close();
+                data = '<html>' + $('html').html() + '</html>'
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.write(data);
+                res.end();
+            });
         });
-        data = '<html>' + $('html').html() + '</html>'
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        res.end();
     });
 });
 
 app.get('/base', function (req, res) {
-    res.send(films);
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db('noc-filmowa');
+        var cursor = dbo.collection('films').aggregate([
+            {
+                $project: {
+                    title: 1,
+                    link: 1,
+                    votes: 1,
+                    votesCount: { $size: '$votes' }
+                }
+            },
+            { $sort: { votesCount: -1 } },
+            { $limit: 500 }
+        ]);
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        var first = true;
+        cursor.on("data", function (data) {
+            if (first) res.write('[' + JSON.stringify(data));
+            else res.write(',\n' + JSON.stringify(data));
+        });
+        cursor.on("end", function () {
+            db.close();
+            res.write(']');
+            res.end();
+        });
+    });
 });
 
 
@@ -73,64 +115,80 @@ app.post('/add', urlencodedParser, function (req, res) {
     var film = {
         title: $("<div>").html(req.body.title).text(),
         link: $("<div>").html(req.body.link).text().replace('"', '').replace(';', ''),
-        votes: {}
+        votes: []
     };
-    films.push(film);
-    res.send('Dodawanie udało się');
-    save();
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db('noc-filmowa');
+        dbo.collection('films').findOne({ title: film.title }, function (err, result) {
+            if (err) throw err;
+            if (result) {
+                db.close();
+                res.send('Już dodano ten film');
+            } else {
+                dbo.collection('films').insertOne(film);
+                db.close();
+                res.send('Dodawanie udało się');
+            }
+        });
+    });
 });
 
 app.get('/validate/:key', function (req, res) {
     var filmTitle = codeToVote.get(req.params.key);
-    console.log(filmTitle);
-    var result = films.filter(function (film) {
-        return film.title === filmTitle;
+
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db('noc-filmowa');
+        dbo.collection('films').findOne({ title: filmTitle }, function (err, result) {
+            if (err) throw err;
+            if (result) {
+                if (result.votes.indexOf(req.params.key) == -1) {
+                    dbo.collection('films').updateOne(
+                        { title: filmTitle },
+                        { $push: { votes: req.params.key } }
+                    );
+                    db.close();
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.write(responseHTML.replace('Wystąpił błąd.', 'Głosowanie udało się.'));
+                    res.end();
+                } else {
+                    db.close();
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.write(responseHTML.replace('Wystąpił błąd.', 'Już na to głosowałaś/głosowałeś!'));
+                    res.end();
+                }
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.write(responseHTML.replace('Wystąpił błąd.', 'Głosowanie nie udało się - nie odnaleziono filmu.'));
+                res.end();
+            }
+        });
     });
-    result.forEach(function (film) {
-        if (film.votes[req.params.key] === undefined) {
-            film.votes[req.params.key] = 1;
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write(responseHTML.replace('Wystąpił błąd.', 'Głosowanie udało się.'));
-            res.end();
-            save();
-        } else {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write(responseHTML.replace('Wystąpił błąd.', 'Już na to głosowałaś/głosowałeś!'));
-            res.end();
-        }
-    });
-    if(result.length == 0) {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write(responseHTML.replace('Wystąpił błąd.', 'Głosowanie nie udało się.'));
-            res.end();
-    }
 });
 
 app.post('/vote', urlencodedParser, function (req, res) {
     var filmTitle = $("<div>").html(req.body.title).text();
     var userMail = req.body.mail;
     if (userMail.match(/^[a-zA-Z0-9.!#$%&'*/=?^_`{|}~-]+@(hotmail.com|gmail.com)$/)) {
-        var hashed = hash(filmTitle+userMail);
+        var hashed = hash(filmTitle + userMail);
         transporter.sendMail(
-        {
-            from: botMailAddress,
-            to: userMail,
-            subject: 'Potwierdzenie głosu na film',
-            html: 'Hej!<br>Jestem botem odpowiedzialnym za głosowanko na filmy, które będą na nocce. Żeby potwierdzić swój głos kliknij tutaj: '
-            + '<a href="' + appAddress + 'validate/' + hashed.toString() + '">' + appAddress + 'validate/'
-            + hashed.toString() + '</a><br>Link jest ważny przez pół godziny więc lepiej się pospiesz.<br>Do zobaczenia na nocy!'
-        }, function (error, info) {
-            if (error) {
-                console.log(error);
-            } else {
-                console.log('Email sent: ' + info.response);
-            }
-        });
+            {
+                from: botMailAddress,
+                to: userMail,
+                subject: 'Potwierdzenie głosu na film',
+                html: 'Hej!<br>Jestem botem odpowiedzialnym za głosowanko na filmy, które będą na nocce. Żeby potwierdzić swój głos kliknij tutaj: '
+                    + '<a href="' + appAddress + 'validate/' + hashed.toString() + '">' + appAddress + 'validate/'
+                    + hashed.toString() + '</a><br>Link jest ważny przez pół godziny więc lepiej się pospiesz.<br>Do zobaczenia na nocy!'
+            }, function (err, info) {
+                if (err) throw err;
+                else console.log('Email sent: ' + info.response);
+            });
         res.send('Wysłano mail z linkiem do potwierdzenia');
         codeToVote.set(hashed.toString(), filmTitle);
         setTimeout(function () {
             codeToVote.delete(hashed);
-        }, 1000*60*30);
+        }, 1000 * 60 * 30);
     } else {
         res.send('Twój adres mailowy jest niepoprawny');
     }
@@ -139,39 +197,6 @@ app.post('/vote', urlencodedParser, function (req, res) {
 var server = app.listen(port, function () {
 });
 
-function save() {
-    MongoClient.connect(url, function (err, db) {
-        if (err) throw err;
-        var dbo = db.db("noc-filmowa");
-        dbo.collection("table").updateOne({}, { $set: { table: films } }, function (err, res) {
-            if (err) throw err;
-            console.log("1 document updated");
-            db.close();
-        });
-    });
-    //fs.writeFile('base.txt', JSON.stringify(films), function (err) {
-    //    if (err) throw err;
-    //    console.log('Done');
-    //});
-}
-
-function read() {
-    MongoClient.connect(url, function (err, db) {
-        if (err) throw err;
-        var dbo = db.db("noc-filmowa");
-        dbo.collection("table").findOne({}, function (err, result) {
-            if (err) throw err;
-            films = result.table;
-            db.close();
-        });
-    });
-    //fs.readFile('base.txt', function (err, data) {
-    //    if (err) throw err;
-    //    films = JSON.parse(data);
-    //    console.log('Done');
-    //});
-}
-
 function hash(str) {
     var h = 0;
     for (var i = 0; i < str.length; i++) {
@@ -179,4 +204,4 @@ function hash(str) {
         h |= 0;
     }
     return h;
-};
+}
