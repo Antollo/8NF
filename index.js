407de342bf40294091fb966741ccf127191ce98d
Antoni Nowinowski <antoninowinowski@hotmail.com>
2018-10-14 20:39:25 +0200

    /rating is more safe

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/captcha', function (req, res) {
-    var captcha = svgCaptcha.create({ size: 8, ignoreChars: 'O0o1il', noise: 0 });
+    var captcha = svgCaptcha.create({ size: 6, ignoreChars: 'O0o1il', noise: 0 });
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({ svg: captcha.data, hash: hash(captcha.data).toString() }));
     captchaToValue.set(hash(captcha.data).toString(), captcha.text);
@@ -88,17 +88,18 @@ app.get('/base', function (req, res) {
 
 
 app.post('/add', urlencodedParser, function (req, res) {
-    var film = {
-        title: $("<div>").html(req.body.title).text(),
-        link: $("<div>").html(req.body.link).text().replace('"', '').replace(';', ''),
-        votes: []
-    };
-    if (film.title.length > 64) {
-        film.title = film.title.substring(0, 64) + '...';
-    }
     if (typeof req.body.title == 'string' && typeof req.body.link == 'string'
         && typeof req.body.hash == 'string' && typeof req.body.value == 'string'
         && req.body.hash.length && req.body.value.length) {
+        var film = {
+            title: $("<div>").html(req.body.title).text(),
+            link: $("<div>").html(req.body.link).text().replace('"', '').replace(';', ''),
+            votes: []
+        };
+        if (film.title.length > 64) {
+            film.title = film.title.substring(0, 64) + '...';
+        }
+
 
         if (captchaToValue.get(req.body.hash) != req.body.value.replace(/\s/g, '')) {
             res.send('Błąd w przepisanych znakach');
@@ -123,83 +124,102 @@ app.post('/add', urlencodedParser, function (req, res) {
             });
         });
     } else {
-        res.send('Błędne zapytanie');
+        res.send('Błąd w zapytaniu');
         console.log('rejected');
     }
 });
 
 app.get('/validate/:key', function (req, res) {
-    var filmTitle = codeToVote.get(req.params.key);
+    //if (typeof req.params.key == 'number') {
+        var filmTitle = codeToVote.get(req.params.key);
 
-    MongoClient.connect(url, function (err, db) {
-        if (err) throw err;
-        var dbo = db.db('noc-filmowa');
-        dbo.collection('films').findOne({ title: filmTitle }, function (err, result) {
+        MongoClient.connect(url, function (err, db) {
             if (err) throw err;
-            if (result) {
-                if (result.votes.indexOf(req.params.key) == -1) {
-                    dbo.collection('films').updateOne(
-                        { title: filmTitle },
-                        { $push: { votes: req.params.key } }
-                    );
-                    db.close();
-                    res.writeHead(200, { 'Content-Type': 'text/html' });
-                    res.write(responseHTML.replace('Wystąpił błąd.', 'Głosowanie udało się.'));
-                    res.end();
+            var dbo = db.db('noc-filmowa');
+            dbo.collection('films').findOne({ title: filmTitle }, function (err, result) {
+                if (err) throw err;
+                if (result) {
+                    if (result.votes.indexOf(req.params.key) == -1) {
+                        dbo.collection('films').updateOne(
+                            { title: filmTitle },
+                            { $push: { votes: req.params.key } }
+                        );
+                        db.close();
+                        res.writeHead(200, { 'Content-Type': 'text/html' });
+                        res.write(responseHTML.replace('Wystąpił błąd.', 'Głosowanie udało się.'));
+                        res.end();
+                    } else {
+                        db.close();
+                        res.writeHead(200, { 'Content-Type': 'text/html' });
+                        res.write(responseHTML.replace('Wystąpił błąd.', 'Już na to głosowałaś/głosowałeś!'));
+                        res.end();
+                    }
                 } else {
-                    db.close();
                     res.writeHead(200, { 'Content-Type': 'text/html' });
-                    res.write(responseHTML.replace('Wystąpił błąd.', 'Już na to głosowałaś/głosowałeś!'));
+                    res.write(responseHTML.replace('Wystąpił błąd.', 'Głosowanie nie udało się - nie odnaleziono filmu.'));
                     res.end();
                 }
-            } else {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.write(responseHTML.replace('Wystąpił błąd.', 'Głosowanie nie udało się - nie odnaleziono filmu.'));
-                res.end();
-            }
+            });
         });
-    });
+    //} else {
+    //    res.write(responseHTML);
+    //}
 });
 
 app.post('/rating', urlencodedParser, function (req, res) {
     var str = '';
-    if (ratingCache.get(req.body.url)) res.send(ratingCache.get(req.body.url));
-    else https.get(req.body.url, function (response) {
-        response.on('data', function (chunk) { str += chunk; });
-        response.on('end', function () {
-            $ = cheerio.load(str);
-            var rating = $('[itemprop="ratingValue"]').text().trim().replace('.', ',');
-            ratingCache.set(req.body.url, rating);
-            res.send(rating);
-        });
-    })
+    if (typeof req.body.url === 'string' && req.body.url.match(/^https:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/)) {
+        if (ratingCache.get(req.body.url)) res.send(ratingCache.get(req.body.url));
+        else {
+            var request = https.get(req.body.url, function (response) {
+                response.on('data', function (chunk) { str += chunk; });
+                response.on('end', function () {
+                    $ = cheerio.load(str);
+                    var rating = $('[itemprop="ratingValue"]').text().trim().replace('.', ',');
+                    ratingCache.set(req.body.url, rating);
+                    res.send(rating);
+                });
+            });
+            request.on('error', function (e) {
+                ratingCache.set(req.body.url, '');
+                res.send('');
+            });
+        }
+    } else {
+        ratingCache.set(req.body.url, '');
+        res.send('');
+    }
 
 });
 
 app.post('/vote', urlencodedParser, function (req, res) {
-    var filmTitle = $("<div>").html(req.body.title).text();
-    var userMail = req.body.mail;
-    if (userMail.match(/^[a-zA-Z0-9.!#$%&'*/=?^_`{|}~-]+@(hotmail.com|gmail.com)$/)) {
-        var hashed = hash(filmTitle + userMail);
-        transporter.sendMail(
-            {
-                from: botMailAddress,
-                to: userMail,
-                subject: 'Potwierdzenie głosu na film',
-                html: 'Hej!<br>Jestem botem odpowiedzialnym za głosowanko na filmy, które będą na nocce. Żeby potwierdzić swój głos kliknij tutaj: '
-                    + '<a href="' + appAddress + 'validate/' + hashed.toString() + '">' + appAddress + 'validate/'
-                    + hashed.toString() + '</a><br>Link jest ważny przez pół godziny więc lepiej się pospiesz.<br>Do zobaczenia na nocy!'
-            }, function (err, info) {
-                if (err) throw err;
-                else console.log('Email sent: ' + info.response);
-            });
-        res.send('Wysłano mail z linkiem do potwierdzenia');
-        codeToVote.set(hashed.toString(), filmTitle);
-        setTimeout(function () {
-            codeToVote.delete(hashed);
-        }, 1000 * 60 * 30);
+    if (typeof req.body.title == 'string' && typeof req.body.mail == 'string') {
+        var filmTitle = $("<div>").html(req.body.title).text();
+        var userMail = req.body.mail;
+        if (userMail.match(/^[a-zA-Z0-9.!#$%&'*/=?^_`{|}~-]+@(hotmail.com|gmail.com)$/)) {
+            var hashed = hash(filmTitle + userMail);
+            transporter.sendMail(
+                {
+                    from: botMailAddress,
+                    to: userMail,
+                    subject: 'Potwierdzenie głosu na film',
+                    html: 'Hej!<br>Jestem botem odpowiedzialnym za głosowanko na filmy, które będą na nocce. Żeby potwierdzić swój głos kliknij tutaj: '
+                        + '<a href="' + appAddress + 'validate/' + hashed.toString() + '">' + appAddress + 'validate/'
+                        + hashed.toString() + '</a><br>Link jest ważny przez pół godziny więc lepiej się pospiesz.<br>Do zobaczenia na nocy!'
+                }, function (err, info) {
+                    if (err) throw err;
+                    else console.log('Email sent: ' + info.response);
+                });
+            res.send('Wysłano mail z linkiem do potwierdzenia');
+            codeToVote.set(hashed.toString(), filmTitle);
+            setTimeout(function () {
+                codeToVote.delete(hashed);
+            }, 1000 * 60 * 30);
+        } else {
+            res.send('Twój adres mailowy jest niepoprawny');
+        }
     } else {
-        res.send('Twój adres mailowy jest niepoprawny');
+        res.send('Błąd w zapytaniu');
     }
 });
 
