4c3fb6142b89e0949c92e859b5718e9864b8db32
Antoni Nowinowski <antoninowinowski@hotmail.com>
2019-11-23 12:27:34 +0100

    2019 maintenance

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
-var mongodb = require('mongodb');
-var MongoClient = mongodb.MongoClient;
-var url = process.env.BASE;
+var mongodb = require('mongodb')
+var MongoClient = mongodb.MongoClient
+var url = process.env.BASE
 
-var express = require('express');
-var app = express();
-var https = require('https');
-var http = require('http').Server(app);
-var fs = require('fs');
+var express = require('express')
+var app = express()
+var https = require('https')
+var fs = require('fs')
 var cheerio = require('cheerio')
-var $ = cheerio.load('');
-var svgCaptcha = require('svg-captcha');
-svgCaptcha.options.width = 240;
-svgCaptcha.options.height = 60;
-var bodyParser = require('body-parser');
-var urlencodedParser = bodyParser.urlencoded({ extended: true });
-var port = process.env.PORT || 3000;
+var $ = cheerio.load('')
+var svgCaptcha = require('svg-captcha')
+svgCaptcha.options.width = 240
+svgCaptcha.options.height = 60
+var bodyParser = require('body-parser')
+var urlencodedParser = bodyParser.urlencoded({ extended: true })
+var port = process.env.PORT || 3000
 
-var nodemailer = require('nodemailer');
-var botMailAddress = 'lenezjastankoska@gmail.com';
-var appAddress = 'https://noc-filmowa.herokuapp.com/';
-//var appAddress = 'http://localhost:3000/';
+var collectionName = 'films2019'
+
+var nodemailer = require('nodemailer')
+var botMailAddress = 'lenezjastankoska@gmail.com'
+
+var appAddress
+if (process.env.PORT)
+    appAddress = 'https://noc-filmowa.herokuapp.com/'
+else
+    appAddress = 'http://localhost:3000/'
 
 var transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -27,37 +32,37 @@ var transporter = nodemailer.createTransport({
         user: botMailAddress,
         pass: process.env.BASE.substring(20, 32)
     }
-});
+})
 
-var codeToVote = new Map();
-var captchaToValue = new Map();
-var ratingCache = new Map();
+var codeToVote = new Map()
+var captchaToValue = new Map()
+var ratingCache = new Map()
 
-var responseHTML;
+var responseHTML
 fs.readFile(__dirname + '/res.html', 'utf8', function (err, data) {
-    if (err) throw err;
-    responseHTML = data;
-    console.log('res.html loaded');
-});
+    if (err) throw err
+    responseHTML = data
+    console.log('res.html loaded')
+})
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/public/main.html');
-});
+    res.sendFile(__dirname + '/public/main.html')
+})
 
 app.get('/captcha', function (req, res) {
-    var captcha = svgCaptcha.create({ size: 6, ignoreChars: 'O0o1il', noise: 0 });
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({ svg: captcha.data, hash: hash(captcha.data).toString() }));
-    captchaToValue.set(hash(captcha.data).toString(), captcha.text);
-});
+    var captcha = svgCaptcha.create({ size: 6, ignoreChars: 'O0o1il', noise: 0 })
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify({ svg: captcha.data, hash: hash(captcha.data).toString() }))
+    captchaToValue.set(hash(captcha.data).toString(), captcha.text)
+})
 
 app.get('/base', function (req, res) {
     MongoClient.connect(url, function (err, db) {
-        if (err) throw err;
-        var dbo = db.db('noc-filmowa');
-        var cursor = dbo.collection('films').aggregate([
+        if (err) throw err
+        var dbo = db.db('noc-filmowa')
+        var cursor = dbo.collection(collectionName).aggregate([
             {
                 $project: {
                     title: 1,
@@ -67,24 +72,24 @@ app.get('/base', function (req, res) {
             },
             { $sort: { votesCount: -1 } },
             { $limit: 100 }
-        ]);
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        var first = true;
-        res.write('[');
+        ])
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        var first = true
+        res.write('[')
         cursor.on("data", function (data) {
             if (first) {
-                res.write(JSON.stringify(data));
-                first = false;
+                res.write(JSON.stringify(data))
+                first = false
             }
-            else res.write(',\n' + JSON.stringify(data));
-        });
+            else res.write(',\n' + JSON.stringify(data))
+        })
         cursor.on("end", function () {
-            db.close();
-            res.write(']');
-            res.end();
-        });
-    });
-});
+            db.close()
+            res.write(']')
+            res.end()
+        })
+    })
+})
 
 
 app.post('/add', urlencodedParser, function (req, res) {
@@ -95,109 +100,109 @@ app.post('/add', urlencodedParser, function (req, res) {
             title: $("<div>").html(req.body.title).text(),
             link: $("<div>").html(req.body.link).text().replace('"', '').replace(';', ''),
             votes: []
-        };
+        }
         if (film.title.length > 64) {
-            film.title = film.title.substring(0, 64) + '...';
+            film.title = film.title.substring(0, 64) + '...'
         }
 
 
         if (captchaToValue.get(req.body.hash) != req.body.value.replace(/\s/g, '')) {
-            res.send('Błąd w przepisanych znakach');
-            console.log('rejected');
+            res.send('Błąd w przepisanych znakach')
+            console.log('rejected')
         }
         else MongoClient.connect(url, function (err, db) {
-            if (err) throw err;
-            var dbo = db.db('noc-filmowa');
-            dbo.collection('films').findOne({ title: film.title }, function (err, result) {
-                if (err) throw err;
+            if (err) throw err
+            var dbo = db.db('noc-filmowa')
+            dbo.collection(collectionName).findOne({ title: film.title }, function (err, result) {
+                if (err) throw err
                 if (result) {
-                    db.close();
-                    res.send('Już dodano ten film');
-                    console.log('rejected');
+                    db.close()
+                    res.send('Już dodano ten film')
+                    console.log('rejected')
                 } else {
-                    dbo.collection('films').insertOne(film);
-                    db.close();
-                    res.send('Dodawanie udało się');
-                    captchaToValue.delete(req.body.hash);
-                    console.log('added');
+                    dbo.collection(collectionName).insertOne(film)
+                    db.close()
+                    res.send('Dodawanie udało się')
+                    captchaToValue.delete(req.body.hash)
+                    console.log('added')
                 }
-            });
-        });
+            })
+        })
     } else {
-        res.send('Błąd w zapytaniu');
-        console.log('rejected');
+        res.send('Błąd w zapytaniu')
+        console.log('rejected')
     }
-});
+})
 
 app.get('/validate/:key', function (req, res) {
     //if (typeof req.params.key == 'number') {
-        var filmTitle = codeToVote.get(req.params.key);
+    var filmTitle = codeToVote.get(req.params.key)
 
-        MongoClient.connect(url, function (err, db) {
-            if (err) throw err;
-            var dbo = db.db('noc-filmowa');
-            dbo.collection('films').findOne({ title: filmTitle }, function (err, result) {
-                if (err) throw err;
-                if (result) {
-                    if (result.votes.indexOf(req.params.key) == -1) {
-                        dbo.collection('films').updateOne(
-                            { title: filmTitle },
-                            { $push: { votes: req.params.key } }
-                        );
-                        db.close();
-                        res.writeHead(200, { 'Content-Type': 'text/html' });
-                        res.write(responseHTML.replace('Wystąpił błąd.', 'Głosowanie udało się.'));
-                        res.end();
-                    } else {
-                        db.close();
-                        res.writeHead(200, { 'Content-Type': 'text/html' });
-                        res.write(responseHTML.replace('Wystąpił błąd.', 'Już na to głosowałaś/głosowałeś!'));
-                        res.end();
-                    }
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err
+        var dbo = db.db('noc-filmowa')
+        dbo.collection(collectionName).findOne({ title: filmTitle }, function (err, result) {
+            if (err) throw err
+            if (result) {
+                if (result.votes.indexOf(req.params.key) == -1) {
+                    dbo.collection(collectionName).updateOne(
+                        { title: filmTitle },
+                        { $push: { votes: req.params.key } }
+                    )
+                    db.close()
+                    res.writeHead(200, { 'Content-Type': 'text/html' })
+                    res.write(responseHTML.replace('Wystąpił błąd.', 'Głosowanie udało się.'))
+                    res.end()
                 } else {
-                    res.writeHead(200, { 'Content-Type': 'text/html' });
-                    res.write(responseHTML.replace('Wystąpił błąd.', 'Głosowanie nie udało się - nie odnaleziono filmu.'));
-                    res.end();
+                    db.close()
+                    res.writeHead(200, { 'Content-Type': 'text/html' })
+                    res.write(responseHTML.replace('Wystąpił błąd.', 'Już na to głosowałaś/głosowałeś!'))
+                    res.end()
                 }
-            });
-        });
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' })
+                res.write(responseHTML.replace('Wystąpił błąd.', 'Głosowanie nie udało się - nie odnaleziono filmu.'))
+                res.end()
+            }
+        })
+    })
     //} else {
-    //    res.write(responseHTML);
+    //    res.write(responseHTML)
     //}
-});
+})
 
 app.post('/rating', urlencodedParser, function (req, res) {
-    var str = '';
+    var str = ''
     if (typeof req.body.url === 'string' && req.body.url.match(/^https:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/)) {
-        if (ratingCache.get(req.body.url)) res.send(ratingCache.get(req.body.url));
+        if (ratingCache.get(req.body.url)) res.send(ratingCache.get(req.body.url))
         else {
             var request = https.get(req.body.url, function (response) {
-                response.on('data', function (chunk) { str += chunk; });
+                response.on('data', function (chunk) { str += chunk; })
                 response.on('end', function () {
-                    $ = cheerio.load(str);
-                    var rating = $('[itemprop="ratingValue"]').text().trim().replace('.', ',');
-                    ratingCache.set(req.body.url, rating);
-                    res.send(rating);
-                });
-            });
+                    $ = cheerio.load(str)
+                    var rating = $('[itemprop="ratingValue"]').text().trim().replace('.', ',')
+                    ratingCache.set(req.body.url, rating)
+                    res.send(rating)
+                })
+            })
             request.on('error', function (e) {
-                ratingCache.set(req.body.url, '');
-                res.send('');
-            });
+                ratingCache.set(req.body.url, '')
+                res.send('')
+            })
         }
     } else {
-        ratingCache.set(req.body.url, '');
-        res.send('');
+        ratingCache.set(req.body.url, '')
+        res.send('')
     }
 
-});
+})
 
 app.post('/vote', urlencodedParser, function (req, res) {
     if (typeof req.body.title == 'string' && typeof req.body.mail == 'string') {
-        var filmTitle = $("<div>").html(req.body.title).text();
-        var userMail = req.body.mail;
+        var filmTitle = $("<div>").html(req.body.title).text()
+        var userMail = req.body.mail
         if (userMail.match(/^[a-zA-Z0-9.!#$%&'*/=?^_`{|}~-]+@(wp.pl|poczta.onet.pl|o2.pl|interia.pl|op.pl|tlen.pl|gmail.com|poczta.fm|gazeta.pl|go2.pl|yahoo.com|hotmail.com|vp.pl|student.put.poznan.pl)$/)) {
-            var hashed = hash(filmTitle + userMail);
+            var hashed = hash(filmTitle + userMail)
             transporter.sendMail(
                 {
                     from: botMailAddress,
@@ -207,30 +212,30 @@ app.post('/vote', urlencodedParser, function (req, res) {
                         + '<a href="' + appAddress + 'validate/' + hashed.toString() + '">' + appAddress + 'validate/'
                         + hashed.toString() + '</a><br>Link jest ważny przez pół godziny więc lepiej się pospiesz.<br>Do zobaczenia na nocy!'
                 }, function (err, info) {
-                    if (err) throw err;
-                    else console.log('Email sent: ' + info.response);
-                });
-            res.send('Wysłano mail z linkiem do potwierdzenia');
-            codeToVote.set(hashed.toString(), filmTitle);
+                    if (err) throw err
+                    else console.log('Email sent: ' + info.response)
+                })
+            res.send('Wysłano mail z linkiem do potwierdzenia')
+            codeToVote.set(hashed.toString(), filmTitle)
             setTimeout(function () {
-                codeToVote.delete(hashed);
-            }, 1000 * 60 * 30);
+                codeToVote.delete(hashed)
+            }, 1000 * 60 * 30)
         } else {
-            res.send('Twój adres mailowy jest niepoprawny');
+            res.send('Twój adres mailowy jest niepoprawny')
         }
     } else {
-        res.send('Błąd w zapytaniu');
+        res.send('Błąd w zapytaniu')
     }
-});
+})
 
 var server = app.listen(port, function () {
-});
+})
 
 function hash(str) {
-    var h = 0;
+    var h = 0
     for (var i = 0; i < str.length; i++) {
-        h = ((h << 5) - h) + str.charCodeAt(i);
-        h |= 0;
+        h = ((h << 5) - h) + str.charCodeAt(i)
+        h |= 0
     }
-    return h;
+    return h
 }
--- public/main.js
+++ public/main.js
@@ -12,103 +12,103 @@ var lineHTML = `<tr>
         <i class="material-icons">thumb_up</i>
     </button>
 </td>
-</tr>`;
-var mail;
+</tr>`
+var mail
 $(document).ready(function () {
     function startTable() {
-        $('#info-card').hide();
-        $('#new-card').hide();
-        $('#ranking-card').show();
-        $('#chart-card').show();
-        $('#floating').show();
-        reloadData();
+        $('#info-card').hide()
+        $('#new-card').hide()
+        $('#ranking-card').show()
+        $('#chart-card').show()
+        $('#floating').show()
+        reloadData()
     }
 
     function reloadData() {
-        if ($('#ranking-card').css('display') == 'none') return;
-        $('#spinner').show();
-        $('#spinner > *').show();
-        $('#darken').show();
+        if ($('#ranking-card').css('display') == 'none') return
+        $('#spinner').show()
+        $('#spinner > *').show()
+        $('#darken').show()
         $.getJSON('base', function (data) {
-            $("#table").empty();
+            $("#table").empty()
             data.forEach(function (film) {
-                var newFilm = $(lineHTML);
-                newFilm.children().eq(0).html('<div class="inner">' + film.title + '</div>');
-                newFilm.children().eq(1).text(film.votesCount);
+                var newFilm = $(lineHTML)
+                newFilm.children().eq(0).html('<div class="inner">' + film.title + '</div>')
+                newFilm.children().eq(1).text(film.votesCount)
                 $.post('rating', { url: film.link }, function (data) {
-                    newFilm.children().eq(2).text(data);
-                });
-                // newFilm.children().eq(2).children().attr('onclick', 'location.href="' + film.link + '";');
-                newFilm.children().eq(3).children().click(function () {
-                    location.href = film.link;
+                    newFilm.children().eq(2).text(data)
                 })
-                //newFilm.children().eq(3).children().attr('onclick', '$.post("vote", {title: "' + film.title + '", mail: mail}, function(data, status){ $(".mdl-js-snackbar")[0].MaterialSnackbar.showSnackbar({message: data});});');
+                // newFilm.children().eq(2).children().attr('onclick', 'location.href="' + film.link + '";')
+                newFilm.children().eq(3).children().click(function () {
+                    location.href = film.link
+                })
+                //newFilm.children().eq(3).children().attr('onclick', '$.post("vote", {title: "' + film.title + '", mail: mail}, function(data, status){ $(".mdl-js-snackbar")[0].MaterialSnackbar.showSnackbar({message: data});});')
                 newFilm.children().eq(4).children().click(function () {
                     $.post('vote',
                         { title: film.title, mail: mail },
                         function (res, status) {
-                            $(".mdl-js-snackbar")[0].MaterialSnackbar.showSnackbar({ message: res });
-                        });
-                });
-                $('#table').append(newFilm);
-            });
-            drawChart(data);
-        });
+                            $(".mdl-js-snackbar")[0].MaterialSnackbar.showSnackbar({ message: res })
+                        })
+                })
+                $('#table').append(newFilm)
+            })
+            drawChart(data)
+        })
     }
 
     function drawChart(input) {
-        google.charts.load('current', { 'packages': ['bar'] });
+        google.charts.load('current', { 'packages': ['bar'] })
         google.charts.setOnLoadCallback(function () {
-            var data = new google.visualization.DataTable();
-            data.addColumn('string', 'Tytuł');
-            data.addColumn('number', 'Głosy');
+            var data = new google.visualization.DataTable()
+            data.addColumn('string', 'Tytuł')
+            data.addColumn('number', 'Głosy')
             for (var i = 0; i < Math.min(10, input.length); i++) {
-                data.addRow([input[i].title, input[i].votesCount]);
+                data.addRow([input[i].title, input[i].votesCount])
             }
             var options = {
                 chart: {},
                 bars: 'horizontal',
                 legend: { position: 'none' },
                 colors: ['rgb(233, 30, 99)']
-            };
-            var chart = new google.charts.Bar(document.getElementById('chart-div'));
-            chart.draw(data, google.charts.Bar.convertOptions(options));
-            $('#spinner').hide();
-            $('#spinner > *').hide();
-            $('#darken').hide();
-        });
+            }
+            var chart = new google.charts.Bar(document.getElementById('chart-div'))
+            chart.draw(data, google.charts.Bar.convertOptions(options))
+            $('#spinner').hide()
+            $('#spinner > *').hide()
+            $('#darken').hide()
+        })
     }
 
     if (Cookies.get('mail') !== undefined) {
-        startTable();
-        mail = Cookies.get('mail');
+        startTable()
+        mail = Cookies.get('mail')
     }
 
     $.getJSON('captcha', function (data) {
-        $('#captcha-img').html(data.svg);
-        $('#captcha-img').attr('hash', data.hash);
-    });
+        $('#captcha-img').html(data.svg)
+        $('#captcha-img').attr('hash', data.hash)
+    })
 
-    setInterval(reloadData, 20000);
+    setInterval(reloadData, 20000)
 
     $('#agree-button').click(function () {
         if ($('#mail').val().length > 0 && !$('#mail').parent().hasClass('is-invalid')) {
-            startTable();
-            Cookies.set('mail', $('#mail').val());
-            mail = $('#mail').val();
-            $('#main-screen').scrollTop(0);
+            startTable()
+            Cookies.set('mail', $('#mail').val())
+            mail = $('#mail').val()
+            $('#main-screen').scrollTop(0)
         } else {
-            $('.mdl-js-snackbar')[0].MaterialSnackbar.showSnackbar({ message: 'Nie podałeś adresu mailowego lub jest nieprawidłowy' });
+            $('.mdl-js-snackbar')[0].MaterialSnackbar.showSnackbar({ message: 'Nie podałeś adresu mailowego lub jest nieprawidłowy' })
         }
-    });
+    })
 
     $('#floating').click(function () {
-        $('#info-card').hide();
-        $('#new-card').show();
-        $('#ranking-card').hide();
-        $('#chart-card').hide();
-        $('#main-screen').scrollTop(0);
-    });
+        $('#info-card').hide()
+        $('#new-card').show()
+        $('#ranking-card').hide()
+        $('#chart-card').hide()
+        $('#main-screen').scrollTop(0)
+    })
 
     $('#add').click(function () {
         if ($("<div>").html($('#title').val()).text().length != 0 && $("<div>").html($('#link').val()).text().length != 0 && $("<div>").html($('#link').val()).text().indexOf('http') != -1 && !$('#link').parent().hasClass('is-invalid')) {
@@ -120,49 +120,82 @@ $(document).ready(function () {
                     value: $('#captcha').val()
                 },
                 function (data, status) {
-                    $('.mdl-js-snackbar')[0].MaterialSnackbar.showSnackbar({ message: data });
+                    $('.mdl-js-snackbar')[0].MaterialSnackbar.showSnackbar({ message: data })
                     if (data == 'Dodawanie udało się') {
-                        $('#back').click();
+                        $('#back').click()
                     }
                     $.getJSON('captcha', function (data) {
-                        $('#captcha-img').html(data.svg);
-                        $('#captcha-img').attr('hash', data.hash);
-                    });
-                });
+                        $('#captcha-img').html(data.svg)
+                        $('#captcha-img').attr('hash', data.hash)
+                    })
+                })
         } else {
-            $('.mdl-js-snackbar')[0].MaterialSnackbar.showSnackbar({ message: 'Tytuł jest pusty lub link nieprawidłowy' });
+            $('.mdl-js-snackbar')[0].MaterialSnackbar.showSnackbar({ message: 'Tytuł jest pusty lub link nieprawidłowy' })
         }
-    });
-
-    $('#back').click(function () {
-        $('#info-card').hide();
-        $('#new-card').hide();
-        $('#ranking-card').show();
-        $('#chart-card').show();
-        $('#title').val('');
-        $('#title').parent().removeClass('is-dirty');
-        $('#link').val('');
-        $('#link').parent().removeClass('is-dirty');
-        $('#link').parent().removeClass('is-invalid');
-        $('#captcha').val('');
-        $('#captcha').parent().removeClass('is-dirty');
-        reloadData();
-    });
-
-    $('.reload').click(function () {
-        reloadData();
     })
 
-    $('#easter-egg').click(function () {
+    $('#back').click(function () {
+        $('#info-card').hide()
+        $('#new-card').hide()
+        $('#ranking-card').show()
+        $('#chart-card').show()
+        $('#title').val('')
+        $('#title').parent().removeClass('is-dirty')
+        $('#link').val('')
+        $('#link').parent().removeClass('is-dirty')
+        $('#link').parent().removeClass('is-invalid')
+        $('#captcha').val('')
+        $('#captcha').parent().removeClass('is-dirty')
+        reloadData()
+    })
 
-        const rules = document.styleSheets[0].cssRules;
+    $('.reload').click(function () {
+        reloadData()
+    })
+
+    /*$('#easter-egg').click(function () {
+
+        const rules = document.styleSheets[0].cssRules
         for (let i = 0; i<rules.length; i++) {
             if ('style' in rules[i]) for (let property in rules[i].style) {
                 if (rules[i].style[property] == 'rgb(19, 79, 92)')
-                rules[i].style[property] = 'rgb(233, 30, 99)';
-            };
+                rules[i].style[property] = 'rgb(233, 30, 99)'
+            }
         }
-        document.styleSheets[0].insertRule('path { fill: rgb(233, 30, 99) !important; }', 0);
-        $('img[src="logo8NF.png"]').css({'filter': 'none'});
+        document.styleSheets[0].insertRule('path { fill: rgb(233, 30, 99) !important; }', 0)
+        $('img[src="logo8NF.png"]').css({'filter': 'none'})
+    })*/
+
+    $('#easter-egg').click(function () {
+        var heart = $(this)
+        var fontSize = parseFloat(heart.css('font-size'))
+
+
+        fontSize = Math.min(fontSize * 1.2, 300)
+
+        if(fontSize == 300)
+        {
+            $(this).stop().animate({
+                fontSize: 14
+            }, {
+                duration: 800,
+                specialEasing: { width: 'linear' }, complete: function () {
+                    heart.show()
+                }
+            })
+        } else {
+
+
+        $(this).stop().animate({
+            fontSize: fontSize
+        }, {
+            duration: 400,
+            specialEasing: { width: 'linear' }, complete: function () {
+                heart.show()
+            }
+        })
+    }
+
+
     })
-});
+})
